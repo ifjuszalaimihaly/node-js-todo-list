9427e052144204dcd62cd5489a2dbb81c884e5ac
Unknown <ifjuszalaimihaly@gmail.com>
2018-05-17 13:31:43 +0200

    can add custom todo

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const mysql = require('mysql');
 
+const bodyParser = require('body-parser');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -18,8 +20,12 @@ connection.connect(function (error) {
     console.log("Mysql connected");
 });
 
-app.get('/add-todo', function (request, response) {
-    var todo = {title: 'Post One', type: 'good'};
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/add-todo', function (request, response) {
+    var title = request.body.title;
+    var type = request.body.type;
+    var todo = {title: title, type: type};
     var sql = 'INSERT INTO todos SET ?';
     var query = connection.query(sql, todo, function (error, result) {
         if (error) throw error;
