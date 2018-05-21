5d4de6bd05e8ce60cedd96ff03b1c61b83f1ac31
Unknown <ifjuszalaimihaly@gmail.com>
2018-05-21 11:23:21 +0200

    work with mongo

--- server/index.js
+++ server/index.js
@@ -1,50 +1,64 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root1234',
-    database: 'todolist'
-});
-
-connection.connect(function (error) {
-    if (error) {
-        throw error;
-    }
-    console.log("Mysql connected");
-});
-
-
+const mongoUrl = "mongodb://localhost:27017";
+ 
+var mongodbClient = require('mongodb').MongoClient;
+ 
 app.use(bodyParser.json());
-
+ 
 app.use(cors());
-
-app.post('/todos', function (request, response) {
-    console.log(request.body);
-    var title = request.body.title;
-    var type = request.body.type;
-    if(title != null && type != null){
-        var todo = {title: title, type: type};
-        var sql = 'INSERT INTO todos SET ?';
-        var query = connection.query(sql, todo, function (error, result) {
-            if (error) throw error;
-            response.send('Added a todo');
+ 
+mongodbClient.connect(mongoUrl,function(err,db){
+    if(err){
+        console.log(err);
+    } else{
+        console.log("connected successfully");
+        var database = db.db("todo-list");
+        database.createCollection('todos', function(err, collection) {
+            if(err){
+                console.log(err);
+            } else {
+                console.log("Collettion created");
+            }
+            db.close();
         });
     }
 });
 
+app.post('/todos', function (request, response) {
+    var title = request.body.title;
+    var type = request.body.type;
+    if(title != null && type != null){
+        var todo = {title: title, type: type};
+        mongodbClient.connect(mongoUrl,function(err,db){
+            var database = db.db("todo-list");
+            var collection = database.collection("todos");
+            collection.insert(todo);
+            db.close();
+        });
+        
+    }
+});
+ 
 app.get('/todos', function (request, response) {
-    var sql = "SELECT * FROM todos";
-    var querey = connection.query(sql, function (err, result, fields) {
-        if (err) throw err;
-    response.send(result);
+    var todos = [];
+    mongodbClient.connect(mongoUrl,function(err,db){
+        var database = db.db("todo-list");
+        var collection = database.collection("todos");
+        var cursor = collection.find();
+        cursor.forEach(function(todo){
+            todos.push(todo);
+        }, function(){
+            response.send(todos);
+            db.close();
+        });
     });
 });
-
+ 
+app.use(express.static('public'));
+ 
 app.listen(3000, function () {
-    console.log('server is running at localhost://3000')
+    console.log('server is running at localhost://3000');
 });
